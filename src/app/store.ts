0ffe4e2d72501723d0f08f9b01d32e962b32b892
oriolraventosc <oriol.raventos.c@gmail.com>
2022-10-31 19:32:39 +0100

    Create and add the task list methods

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+import { tasksListReducer } from "../redux/features/listSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    taskListMethods: tasksListReducer,
   },
 });
 
