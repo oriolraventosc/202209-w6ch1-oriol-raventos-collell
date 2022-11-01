eabb4d7b4c959247c7432ef6591cfc4145258dcd
oriolraventosc <oriol.raventos.c@gmail.com>
2022-11-01 01:25:34 +0100

    Add list test

---
+++ src/components/List.test.tsx
@@ -0,0 +1,22 @@
+import List from "./list";
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../app/store";
+
+describe("Given a List component", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show a list of tasks", () => {
+      render(
+        <Provider store={store}>
+          <List />
+        </Provider>
+      );
+
+      const ulList = screen.queryByRole("list", {
+        name: "task",
+      });
+
+      expect(ulList).toBeInTheDocument();
+    });
+  });
+});
