5e4237a8e0c885fb944cb7ee7115f2e7e3832e4e
oriolraventosc <oriol.raventos.c@gmail.com>
2022-11-02 01:23:00 +0100

    Create mocks archive and add mockTasksList

---
+++ src/mocks/mockTasksList.ts
@@ -0,0 +1,21 @@
+import types from "../types";
+
+const mockTasksList: types[] = [
+  {
+    id: 0,
+    name: "Sleep 1h per day",
+    done: false,
+  },
+  {
+    id: 1,
+    name: "Review header pull request",
+    done: false,
+  },
+  {
+    id: 2,
+    name: "Start learning bootstrap",
+    done: false,
+  },
+];
+
+export default mockTasksList;
